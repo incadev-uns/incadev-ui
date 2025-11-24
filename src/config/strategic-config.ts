87c52cad607fa83c7d8c5077f2ad6cd5fddc67d1
AlexDeRivia <alexalcantaraz1809@gmail.com>
2025-11-24 07:18:59 -0500

    cambios

--- src/config/strategic-config.ts
+++ src/config/strategic-config.ts
@@ -7,9 +7,9 @@
 type apiProps = string | number;
 
 export const config = {
-  //apiUrl:
-  //  "https://instituto.cetivirgendelapuerta.com/estrategico/StrategicProcessApp/public",
-  apiUrl: "http://127.0.0.1:8001",
+  apiUrl:
+  "https://instituto.cetivirgendelapuerta.com/estrategico/StrategicProcessApp/public",
+  //apiUrl: "http://127.0.0.1:8001",
   environment: "development",
   endpoints: {
     messageFiles: {
