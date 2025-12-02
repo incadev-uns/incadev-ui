a389cdc2a45dab30128ee2a4ccb26692381d1e88
2Mari47 <mariestherloma@gmail.com>
2025-12-01 19:38:17 -0500

    cambio

--- src/process/administrative/indicators/Indicadores.tsx
+++ src/process/administrative/indicators/Indicadores.tsx
@@ -1,3 +1,4 @@
+//
 import React, { useState, useEffect } from 'react';
 import AdministrativeLayout from '@/process/administrative/AdministrativeLayout';
 import { config } from "@/config/administrative-config";
