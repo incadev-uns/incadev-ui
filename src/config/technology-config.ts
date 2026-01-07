46dbbb0655c8ca7f6cbcfd4fa93e4edd330f7866
NiltonRamosE <niltonencarnacion17@gmail.com>
2026-01-06 22:39:03 -0500

    feat: technology config api

--- src/config/technology-config.ts
+++ src/config/technology-config.ts
@@ -5,7 +5,7 @@
  **/
 
 export const config = {
-  apiUrl: "https://project-48be602c-d967-4478-ab6.uc.r.appspot.com/api",
+  apiUrl: "http://35.232.144.43/api",
   //apiUrl: "http://localhost:8000/api",
   environment: "development",
   endpoints: {
